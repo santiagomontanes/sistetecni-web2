cc4be641c4dd65d2688de2490e56335da6f4cc2c
santiagomontanes <vasquezsantiagotc58@gmail.com>
2026-01-28 23:54:46 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://whicjkdnzafebphsuyas.supabase.co",
+        hostname: "whicjkdnzafebphsuyas.supabase.co",
         pathname: "/storage/v1/object/**",
       },
     ],
